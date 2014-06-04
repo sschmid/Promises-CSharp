c65f4b12e10b0dbe9c9b520ef9b499c7055d384e
Simon Schmid <mail@sschmid.com>
2014-06-04 18:43:42 +0200

    Added Unity project

---
+++ Promises-Unity/Assets/Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Threading;
+using Promises;
+
+public class Controller : MonoBehaviour {
+    void Start() {
+        PromiseBehaviour.PromiseWithAction(run, onStateChanged);
+    }
+
+    string run() {
+        Thread.Sleep(2000);
+        return "Hello from a promise";
+    }
+
+    void onStateChanged(Promise<object> promise) {
+        Debug.Log(promise.result);
+        new GameObject(promise.result as string);
+    }
+}
---
+++ Promises-Unity/Assets/MyPromise.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Threading;
+
+public class MyPromise : PromiseBehaviour {
+    void Awake() {
+        Init(() => {
+            Thread.Sleep(2000);
+            return "Hello from MyPromise";
+        }, promise => {
+            Debug.Log(promise.result);
+            new GameObject((string)promise.result);
+        });
+    }
+}
